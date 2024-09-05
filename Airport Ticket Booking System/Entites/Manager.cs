1f4c51d502453b4f6f83ee468e8efbcc11b64c86
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-09-06 00:43:15 +0300

    Manage Folders

---
+++ Airport Ticket Booking System/Entites/Manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Entites;
+    public class Manager
+    {
+    }
+
---
+++ Airport Ticket Booking System/Program.cs
@@ -0,0 +1,26 @@
+﻿using Airport_Ticket_Booking_System.Entites.BookingManagement;
+using Airport_Ticket_Booking_System.Entites.FlightManagment;
+using Airport_Ticket_Booking_System.Entites.PassengersManager;
+using System.Security.Cryptography;
+
+namespace Airport_Ticket_Booking_System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PassenegersManager.AddAPassenger("Raghad");
+
+
+        BookingManager.ShowAvailableFlights();
+
+        Passenger p = PassenegersManager.Passengers.FirstOrDefault(p => p.Name == "Raghad");
+        Console.WriteLine($"\n\n");
+        BookingManager.BookAFlight(ClassOfFlight.Business, 5, p.Id);
+        BookingManager.BookAFlight(ClassOfFlight.Economy, 11, p.Id);
+        BookingManager.BookAFlight(ClassOfFlight.FirstClass, 14, p.Id);
+        p.ShowBooking();
+
+        BookingManager.CancelAbooking(1, p.Id);
+        p.ShowBooking();
+    }
+}
