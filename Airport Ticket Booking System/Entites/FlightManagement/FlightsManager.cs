3696f4e124ce5d05a9ce68393a952bf3de66608d
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-09-06 16:28:08 +0300

    Remove Data from FlightsManager

--- Airport Ticket Booking System/Entites/FlightManagement/FlightsManager.cs
+++ Airport Ticket Booking System/Entites/FlightManagement/FlightsManager.cs
@@ -11,32 +11,7 @@ using System.Security.Claims;
 namespace Airport_Ticket_Booking_System.Entites.FlightManagment;
 public class FlightsManager
 {
-    private static List<Flight> flights = new List<Flight>()
-
-        {
-                new Flight(300, 700, 1200, "USA", "UK", new DateTime(2024, 10, 15), "JFK", "LHR"),
-new Flight(350, 800, 1300, "France", "Germany", new DateTime(2024, 11, 5), "CDG", "FRA"),
-new Flight(650, 900, 1400, "Italy", "Canada", new DateTime(2024, 12, 20), "FCO", "YYZ"),
-new Flight(280, 650, 1150, "Japan", "Brazil", new DateTime(2024, 9, 10), "HND", "GRU"),
-new Flight(320, 720, 1250, "Australia", "India", new DateTime(2024, 8, 12), "SYD", "DEL"),
-new Flight(290, 680, 1180, "Germany", "France", new DateTime(2024, 7, 18), "FRA", "CDG"),
-new Flight(310, 760, 1270, "UK", "USA", new DateTime(2024, 5, 22), "LHR", "JFK"),
-new Flight(340, 810, 1320, "Brazil", "Italy", new DateTime(2024, 4, 9), "GRU", "FCO"),
-new Flight(370, 880, 1350, "Canada", "Australia", new DateTime(2024, 6, 15), "YYZ", "SYD"),
-new Flight(350, 650, 1210, "India", "Japan", new DateTime(2024, 3, 1), "DEL", "HND"),
-new Flight(330, 770, 1260, "France", "UK", new DateTime(2024, 2, 12), "CDG", "LHR"          ),
-new Flight(360, 850, 1370, "Italy", "Germany", new DateTime(2024, 1, 22), "FCO", "FRA"),
-new Flight(295, 700, 1200, "Australia", "USA", new DateTime(2024, 12, 5), "SYD", "JFK"),
-new Flight(325, 790, 1290, "Germany", "Canada", new DateTime(2024, 11, 9), "FRA", "YYZ"),
-new Flight(650, 830, 1330, "Japan", "France", new DateTime(2024, 10, 30), "HND", "CDG"),
-new Flight(305, 720, 1190, "Brazil", "India", new DateTime(2024, 9, 18), "GRU", "DEL"),
-new Flight(350, 780, 1280, "Canada", "UK", new DateTime(2024, 8, 14), "YYZ", "LHR"),
-new Flight(375, 890, 1390, "USA", "Australia", new DateTime(2024, 7, 25), "JFK", "SYD"),
-new Flight(290, 700, 1220, "India", "Italy", new DateTime(2024, 6, 11), "DEL", "FCO"),
-new Flight(310, 760, 1300, "France", "Brazil", new DateTime(2024, 5, 7), "CDG", "GRU")
-
-        };
-
+    private static List<Flight> flights = new List<Flight>();
 
 
     public static List<Flight> GetAllFlights()
